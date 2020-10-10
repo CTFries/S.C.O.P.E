fb334df138b0166e4dd88a9b631d3b9de6971ce7
LeviRoss2 <30361605+LeviRoss2@users.noreply.github.com>
2020-10-10 18:23:51 -0500

    Add files via upload

--- commands/eits.js
+++ commands/eits.js
@@ -2,18 +2,18 @@ const numeral = require("numeral");
 const races = require("../resources/units");
 
 function raceIdentify(i) {
-  let words = i.slice(i.length - 1).toString().split('\n').slice(1).toString().split(':').toString().split(',').toString();
-  if (words.search("Knights:") > 0) {
+  let words = i.toString();
+  if (words.search("Catapults") > 0) {
     return "human";
-  } else if (words.search("Ghosts") > 0) {
+  } else if (words.search("Archmages") > 0) {
     return "elf";
-  } else if (words.search("Ogres") > 0) {
+  } else if (words.search("Nazgul") > 0) {
     return "orc";
-  } else if (words.search("Axemen") > 0) {
+  } else if (words.search("Cavemasters") > 0) {
     return "dwarf";
-  } else if (words.search("Warlords") > 0) {
+  } else if (words.search("Berserkers") > 0) {
     return "troll";
-  } else if (words.search("Farmers") > 0) {
+  } else if (words.search("Adventurers") > 0) {
     return "halfling";
   } else {
     return "Error";
@@ -28,25 +28,72 @@ module.exports = (client, message, args) => {
   let military = 0;
   let magic = 0;
   let units = [];
+  let rawUnits = 0;
+  let rawUnknown = 0;
+  let rawGTs = 0;
+  let GTs = 0;
+  let extraArmies = 0;
+  let extraTroops = 0;
+  let tempArray = 'N/A',Type = 'N/A', interm = 'N/A', Owner = 'N/A', Target='N/A', tempEndOwner='N/A', startTarget='N/A', endTarget='N/A', startOwner='N/A', endOwner='N/A', tempOwner = 'N/A';
 
-  if(args[0] > 0 && args [1] > 0){
+  if (args[0] >= 0 && args[1] >= 0) {
     military = args[0];
     magic = args[1];
-  }
-  
-  let allWords = args.toString().split('\n').slice(1).toString().split(':').toString().split(',');
+    tempArray = args.slice(2,args.length).toString().split(`,`);
+  }else{tempArray = args.toString().split(`,`);}
 
-  let count = 0;
-  for(i=0; i<(allWords.length/2); i++){
-    units[i] = allWords[(i*2)+1];
+  console.log(args);
+  console.log(args.length);
+
+  // Army EITS reports (short)
+  if (args.length < 20) {
+
+    Type = 'Army';
+    rawUnits = args.toString().split('\n').slice(1).toString().split(':').toString().split(',');
+    rawNames = args.toString().split('\n').slice(0,1).toString().split(',');
+
+    interm = rawNames.indexOf('from');
+    Owner = rawNames.slice(interm+1).toString().split(',').join(' ');
+
+    Target = rawNames.slice(0, interm).toString().split(',').join(' ');
+  }
+  // City EITS reports
+  else {
+
+    Type = 'City';
+    rawUnits = args.toString().split(`\n`).slice(1, 7).toString().split(`,`).toString().split(`:`).toString().split(`,`);
+    rawGTs = args.toString().split(`\n`).slice(12, 13).toString().split(`,`).slice(1, 2).toString().split(`:`);
+    rawUnknown = args.toString().split(`\n`).slice(19, 20).toString().split(`,`);
+    GTs = rawGTs[1];
+    extraArmies = parseInt(rawUnknown[4]);
+    extraTroops = parseInt(rawUnknown[6]);
+
+    rawUnits.push("GTs", GTs);
+
+    startTarget = tempArray.indexOf('about');
+    endTarget = tempArray.indexOf('');
+    startOwner = tempArray.indexOf('by');
+    // last part of Owner name
+    tempEndOwner = tempArray[tempArray.length - 11].split(`\n`)[0].replace(':', '');
+    endOwner = tempArray.length - 11;
+
+    Target = tempArray.slice(startTarget + 1, endTarget).join(' ');
+    tempOwner = tempArray.slice(startOwner + 1, endOwner);
+    tempOwner.push(tempEndOwner);
+
+    Owner = tempOwner.join(' ');
+  }
+
+  for (i = 0; i < (rawUnits.length / 2); i++) {
+    units[i] = rawUnits[(i * 2) + 1];
   }
 
   // Identifies Race
-  const raceName = raceIdentify(args);
+  const raceName = raceIdentify(rawUnits);
   const race = races[raceName];
 
   // Handles elf mess
-  if (race.u5.name === "Archmages") {
+  if (raceName === "elf") {
     let mag = magic > 9 ? 9 : magic;
     race.u5.op = mag * 3;
     race.u5.dp = mag * 3;
@@ -56,39 +103,83 @@ module.exports = (client, message, args) => {
     let unitI = "u".concat(i);
     if (i < 7) {
       // units are base 0, so subtract 1
-      op = isNaN(units[i-1]) ? op : op + units[i-1] * race[unitI].op;
-      dp = isNaN(units[i-1]) ? dp : dp + units[i-1] * race[unitI].dp;
-      cost = isNaN(units[i-1]) ? cost : cost + units[i-1] * race[unitI].cost;
+      op = isNaN(units[i - 1]) ? op : op + units[i - 1] * race[unitI].op;
+      dp = isNaN(units[i - 1]) ? dp : dp + units[i - 1] * race[unitI].dp;
+      cost = isNaN(units[i - 1]) ? cost : cost + units[i - 1] * race[unitI].cost;
     }
   }
   op = military ? op * (1 + military / 10) : op;
   dp = military ? dp * (1 + military / 10) : dp;
-  dp = units[6] > 0 ? dp + units[6] * (5 + military) : dp;  // likewise, 6 would be GTs now, not 7
+  dp = units[6] > 0 ? dp + units[6] * (5 + parseInt(military)) : dp;  // likewise, 6 would be GTs now, not 7
 
-  //Output results
-  message.channel.send({
-    embed: {
-      color: 2123412,
-      description: ` EITS Requested by: ${message.author}\n
+  if (Type == 'City') {
+    //Output results
+    message.channel.send({
+      embed: {
+        color: 2123412,
+        description: ` EITS Requested by: ${message.author}\n
+            **Target ${Type}:**
+            ${Target}
+            **Owner:**
+            ${Owner}
+
             **Units**
-            ${race.u1.name}: ${numeral(units[0]).format("0,0")}\n${race.u2.name
-        }: ${numeral(units[1]).format("0,0")}\n${race.u3.name}: ${numeral(
-          units[2]
-        ).format("0,0")}\n${race.u4.name}: ${numeral(units[3]).format(
-          "0,0"
-        )}\n${race.u5.name}: ${numeral(units[4]).format("0,0")}\n${race.u6.name
-        }: ${numeral(units[5]).format("0,0")}\n${race.u7.name}: ${numeral(
-          units[6]
-        ).format("0,0")}
-            \n**Power**
-            Military: ${military} \nMagic: ${magic} \nCost: ${numeral(
-          cost
-        ).format("0,0")}\nOP: ${numeral(op).format("0,0.0")}\nDP: ${numeral(
-          dp
-        ).format("0,0.0")}
-            \n **credit**
+            ${race.u1.name}: ${numeral(units[0]).format("0,0")}
+            ${race.u2.name}: ${numeral(units[1]).format("0,0")}
+            ${race.u3.name}: ${numeral(units[2]).format("0,0")}
+            ${race.u4.name}: ${numeral(units[3]).format("0,0")}
+            ${race.u5.name}: ${numeral(units[4]).format("0,0")}
+            ${race.u6.name}: ${numeral(units[5]).format("0,0")}
+            ${race.u7.name}: ${numeral(units[6]).format("0,0")}
+            Extra Armies: ${numeral(extraArmies).format("0")}
+            Extra Troops: ${numeral(extraTroops).format("0,0")}
+            
+            **Power**
+            Military Sci: ${military}
+            Magic Sci: ${magic}
+            Cost: ${numeral(cost).format("0,0")}
+            OP: ${numeral(op).format("0,0.0")}
+            DP: ${numeral(dp).format("0,0.0")}
+            
+            **Credit**
             made with :heart: by Percy & Moff`,
-    },
-  });
+      },
+    });
+    message.delete({ timeout: 1000 });
+
+  } else {
+
+    //Output results
+    message.channel.send({
+      embed: {
+        color: 2123412,
+        description: ` EITS Requested by: ${message.author}\n
+            **Target ${Type}:**
+            ${Target}
+            **Owner:**
+            ${Owner}
+
+            **Units**
+            ${race.u1.name}: ${numeral(units[0]).format("0,0")}
+            ${race.u2.name}: ${numeral(units[1]).format("0,0")}
+            ${race.u3.name}: ${numeral(units[2]).format("0,0")}
+            ${race.u4.name}: ${numeral(units[3]).format("0,0")}
+            ${race.u5.name}: ${numeral(units[4]).format("0,0")}
+            ${race.u6.name}: ${numeral(units[5]).format("0,0")}
+            
+            **Power**
+            Military Sci: ${military}
+            Magic Sci: ${magic}
+            Cost: ${numeral(cost).format("0,0")}
+            OP: ${numeral(op).format("0,0.0")}
+            DP: ${numeral(dp).format("0,0.0")}
+            
+            **Credit**
+            made with :heart: by Percy & Moff`,
+      },
+    });
+    message.delete({ timeout: 1000 });
+
+  }
 
 }
